7a898368d9e9e00b6168011d982289ec41a1f11d
UHeim <inlinefunc1812@gmail.com>
2019-09-06 03:50:24 +0900

    Set Custom Cursor

---
+++ Assets/Scripts/Utils/MouseCursor.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+enum MouseState
+{
+
+}
+
+public class MouseCursor : MonoBehaviour
+{
+    public Texture2D cursor;
+
+    public bool fixPointIsCenter;
+    public Vector2 fixPointAdjust;
+
+    private Vector2 hotspot;
+
+    private void Awake()
+    {
+        StartCoroutine("SetCursor");
+    }
+
+    private IEnumerator SetCursor()
+    {
+        yield return new WaitForEndOfFrame();
+
+        if (fixPointIsCenter)
+        {
+            hotspot.x = cursor.width / 2;
+            hotspot.y = cursor.height / 2;
+        }
+        else
+        {
+            hotspot = fixPointAdjust;
+        }
+
+        Cursor.SetCursor(cursor, hotspot, CursorMode.Auto);
+
+        yield break;
+    }
+}
